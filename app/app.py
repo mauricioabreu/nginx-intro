ed7153d98aa2bbe0c98a6eb4571b55d4c799931f
Maurício Antunes <mauricio.abreua@gmail.com>
2022-05-20 19:10:52 -0300

    Add cache for new route

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<p>Index page!</p>"
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "nobody")
+    return f"<p>Hello, {name}</p>"
